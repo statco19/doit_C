5f0df5416cafbf7a3c5d9b1ac16bb3b856aa5b9c
Stephen <statco19@gmail.com>
2020-10-13 19:45:01 +0900

    Add files via upload
    10/13

---
+++ ch11/global_variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int result;
+
+void Sum(int data1, int data2) {
+	result = data1 + data2;
+}
+
+void main(){
+    
+    Sum(5,3);
+	printf("5 + 3 = %d\n", result);
+
+}
---
+++ ch11/local_and _global.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int result;
+
+void Sum(int data1, int data2) {
+
+	int result;
+	result = data1 + data2;
+}
+
+void main(){
+	
+
+    Sum(5,3);
+	printf("5 + 3 = %d\n", result);
+}
