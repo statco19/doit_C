4e2df1d57e8ea5a9bf4a1d53095b4dcdd2ef072f
Stephen <statco19@gmail.com>
2020-12-26 13:50:37 +0900

    Add files via upload
    12/26

---
+++ ch18/Friends_Struct.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#define MAX_C 5
+
+typedef struct People{
+	char name[14];
+	unsigned short int age;
+	float height;
+	float weight;
+} Person;
+
+int AddFriend(Person *p_friend, int count){
+	if(count<MAX_C) {
+		p_friend = p_friend + count;
+		printf("\n새로운 친구 정보를 입력하세요.\n");
+		printf("1. 이름 : \n");
+		// 필요한 건 주소
+		scanf("%s", p_friend->name);
+		// &(*p_friend).name[0]
+		// (*p_friend).name
+		printf("2. 나이 : \n");
+		scanf("%hu", &p_friend->age);
+		// &(*p_friend).age
+		printf("3. 키 : \n");
+		scanf("%f", &p_friend->height);
+		// &(*p_friend).height
+		printf("4. 몸무게 : \n");
+		// &(*p_friend).weight
+		scanf("%f", &p_friend->weight);
+		printf("입력을 완료했습니다.\n");
+		
+		return 1;
+	} else {
+		printf("최대 인원을 초과하여 입력을 할 수가 없습니다.\n");
+		printf("최대 %d명까지만 관리 가능합니다. \n\n",MAX_C);
+	}
+	return 0;
+}
+
+void ShowFriendList(Person *p_friend, int count) {
+	int i;
+	if(count > 0) {
+		printf("\n등록된 친구 목록\n");
+		printf("=========================\n");
+		for(i=0;i<count;i++){
+			printf("%-14s, %3d세, %6.2fcm, %6.2fkg\n", p_friend->name, p_friend->age, p_friend->height, p_friend->weight); 
+			// 필요한 건 값
+			p_friend++;
+		}
+		printf("=========================\n\n");
+	} else {
+		printf("\n등록된 친구가 없습니다. \n\n");
+	}
+}
+
+void main(){
+	Person friends[MAX_C];
+   int count = 0, num;
+   while(1) {
+   		printf("	[메뉴]	\n");
+		printf("======================\n");
+		printf("1. 친구 추가		\n");
+		printf("2. 친구 목록 보기 \n");
+		printf("3. 종료 		\n");
+		printf("======================\n");
+		printf("번호 선택 : \n");
+		scanf("%d", &num);
+		
+   		if(num==1){
+			if(1==AddFriend(friends, count)) count++;
+		} else if(num==2){
+			ShowFriendList(friends, count);
+		} else if(num==3){
+			break;
+		}
+		else printf("1~3 번호만 선택할 수 있습니다!!\n\n");
+   }
+
+}
